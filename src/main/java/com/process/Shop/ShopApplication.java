a7e8b0a03879974ab4d8c1517f19168b21051e33
Cristian <cristiaykarol1986@gmail.com>
2024-02-29 21:30:31 -0500

    init commit

---
+++ src/main/java/com/process/Shop/ShopApplication.java
@@ -0,0 +1,23 @@
+package com.process.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+	@GetMapping("hello")
+	public static String helloWord(){
+		//System.out.println("hello mundo");
+        return "hello mundo";
+    }
+
+}
---
+++ src/test/java/com/process/Shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.process.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
