3b7e2177c5125043c179aab94acf555f3c023143
cristian <cristiaykarol1986@gmail.com>
2024-04-18 19:56:13 -0500

    Implementacion sobre ResponseEntity

--- src/main/java/com/process/shop/controller/UserController.java
+++ src/main/java/com/process/shop/controller/UserController.java
@@ -3,6 +3,8 @@ package com.process.shop.controller;
 import com.process.shop.model.User;
 import com.process.shop.service.User.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +17,35 @@ public class UserController {
 
     // Endpoint para crear un nuevo usuario
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userService.createUser(user));
     }
 
     // Endpoint para obtener todos los usuarios
     @GetMapping
-    public List<User> getAllUsers() {
-        return userService.findAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity
+                .ok()
+                .body(userService.findAllUsers());
     }
 
     // Endpoint para obtener un usuario por su ID
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity
+                .ok()
+                .body(userService.getUserById(id));
     }
 
 
     // Endpoint para actualizar un usuario existente
     @PutMapping("/{id}")
-    public User updateUser(@PathVariable Long id, @RequestBody User user) {
-        return userService.updateUser(user,id);
+    public ResponseEntity<User>updateUser(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity
+                .ok()
+                .body(userService.updateUser(user,id));
     }
 
     // Endpoint para eliminar un usuario por su ID
